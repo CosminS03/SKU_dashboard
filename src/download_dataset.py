cf52838302fcaee2f4fc5d698c0e16d7132411a4
Cosmin Spanu <cosminspanu03@gmail.com>
2025-09-13 21:36:53 +0300

    Fixed download_dataset file

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -2,10 +2,11 @@ from dotenv import load_dotenv
 import os
 import kagglehub
 import shutil
+import config
 
 load_dotenv()
-kaggle_api_key = os.environ.get("KAGGLE_API_KEY")
-download_path = input("Enter the path into which you want to download the dataset: ")
+kaggle_api_key = config.kaggle_api_key
+download_path = "./data/raw"
 os.environ["KAGGLEHUB_CACHE"] = download_path
 dataset_path = kagglehub.dataset_download(
     "thedevastator/online-retail-sales-and-customer-data"
@@ -15,6 +16,5 @@ source_path = (
     download_path
     + "/datasets/thedevastator/online-retail-sales-and-customer-data/versions/1/online_retail.csv"
 )
-destination_path = "./data/raw"
-shutil.copy(source_path, destination_path)
+shutil.copy(source_path, download_path)
 shutil.rmtree(download_path + "/datasets")
